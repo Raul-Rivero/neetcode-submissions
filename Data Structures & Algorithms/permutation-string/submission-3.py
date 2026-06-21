e234866f7a92cad86ece2858a9498a4a0a03c051
Raul Rivero <108901703+Raul-Rivero@users.noreply.github.com>
2026-06-21 18:19:46 -0400

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        map_s1 = defaultdict(int)
+        for i in s1:
+            map_s1[i] += 1
+
+        l = 0
+        r = l + len(s1)
+
+        while r < len(s2)+1:
+            map_temp = defaultdict(int)
+            r = l + len(s1)
+            for i in s2[l:r]:
+                map_temp[i] += 1
+            if map_temp == map_s1:
+                return True
+            l += 1
+
+        return False
