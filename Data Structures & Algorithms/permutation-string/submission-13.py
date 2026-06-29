ad01aff0fd71886992041a24e6caae89c40018c1
Raul Rivero <108901703+Raul-Rivero@users.noreply.github.com>
2026-06-29 11:08:48 -0400

    Add: permutation-string - submission-13

---
+++ Data Structures & Algorithms/permutation-string/submission-13.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+
+        goal = defaultdict(int)
+
+        for i in s1:
+            goal[i] += 1
+
+        l = 0
+        r = l + len(s1)
+
+        while r < len(s2) + 1:
+            r = l + len(s1)
+            temp = defaultdict(int)
+            for i in s2[l:r]:
+                temp[i] += 1
+            if temp == goal:
+                return True
+            
+            l += 1
+
+        return False
