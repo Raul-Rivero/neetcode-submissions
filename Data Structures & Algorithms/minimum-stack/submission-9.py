b943ab919534e7dfc644fc48ddd89610830c957e
Raul Rivero <108901703+Raul-Rivero@users.noreply.github.com>
2026-07-03 11:07:12 -0400

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.MainStack = []
+        self.MinStack = []
+
+    def push(self, val: int) -> None:
+        self.MainStack.append(val)
+        if self.MinStack:
+            val = min(val, self.MinStack[-1])
+        self.MinStack.append(val)
+
+    def pop(self) -> None:
+        self.MainStack.pop()
+        self.MinStack.pop()
+
+    def top(self) -> int:
+        return self.MainStack[-1]
+
+    def getMin(self) -> int:
+        return self.MinStack[-1]
