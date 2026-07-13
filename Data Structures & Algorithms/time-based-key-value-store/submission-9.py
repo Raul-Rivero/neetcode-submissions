6351c259058c57a4992bc2e8999895ae5427566a
Raul Rivero <108901703+Raul-Rivero@users.noreply.github.com>
2026-07-13 10:59:03 -0400

    Add: time-based-key-value-store - submission-9

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-9.py
@@ -0,0 +1,36 @@
+class TimeMap:
+
+    def __init__(self):
+        self.TimeMap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.TimeMap[key].append([value,timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+        res = ""
+
+        if key not in self.TimeMap:
+            return res
+
+        log = self.TimeMap[key]
+
+        l,r = 0, len(log) - 1
+
+        while l <= r:
+
+            m = (l + r) // 2 
+
+            if log[m][1] == timestamp:
+                res = log[m][0]
+                break
+
+            if log[m][1] < timestamp:
+                res = log[m][0]
+                l = m + 1
+            else:
+                
+                r = m - 1
+    
+        return res
+        
