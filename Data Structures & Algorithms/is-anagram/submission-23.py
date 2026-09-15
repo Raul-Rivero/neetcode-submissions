4ce34487ead96ade5b565b5aedd41eaf94c69141
Raul Rivero <108901703+Raul-Rivero@users.noreply.github.com>
2026-09-14 23:01:57 -0400

    Add: is-anagram - submission-23

---
+++ Data Structures & Algorithms/is-anagram/submission-23.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        dict1 = defaultdict(int)
+        dict2 = defaultdict(int)
+
+        for i in s:
+            dict1[i] += 1
+
+        for i in t:
+            dict2[i] += 1
+
+        return dict1 == dict2
+            
